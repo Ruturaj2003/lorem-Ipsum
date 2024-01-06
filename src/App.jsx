b1b6d556e9e3942a8502859d974e476f255f4c06
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-06 19:19:19 +0530

    Finish Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,51 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import data from './data';
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(typeof parseInt(count));
+    let amt = parseInt(count);
+    setCount(amt);
+    setText(data.slice(0, count));
+  };
+  return (
+    <section className="section-center">
+      <h1>
+        Lorem Ipsum Pro{' '}
+        <b>
+          <i> Max Ultra</i>
+        </b>{' '}
+      </h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h4>Silly Lorem and Sillier Ipsum</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Num of Paras:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amout"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          {' '}
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
